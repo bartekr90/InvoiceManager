1587899d71640084ee4811296e82de5b0e299ef3
Bartosz Rachwał <bartekr90@gmail.com>
2022-05-02 21:42:04 +0200

    data validation

--- InvoiceManager/Controllers/HomeController.cs
+++ InvoiceManager/Controllers/HomeController.cs
@@ -95,6 +95,12 @@ namespace InvoiceManager.Controllers
             var userId = User.Identity.GetUserId();
             invoice.UserId = userId;
 
+            if (!ModelState.IsValid)
+            {
+                var vm = PrepareInvoiceVm(invoice, userId);
+                return View("Invoice", vm);
+            }
+
             if (invoice.Id == 0)
                 _invoiceRepository.Add(invoice);
             else
@@ -109,6 +115,12 @@ namespace InvoiceManager.Controllers
             var userId = User.Identity.GetUserId();
 
             var product = _productRepository.GetProduct(invoicePosition.ProductId);
+
+            if (!ModelState.IsValid)
+            {
+                var vm = PrepareInvoicePositionVm(invoicePosition);
+                return View("InvoicePosition", vm);
+            }
 
             invoicePosition.Value = invoicePosition.Quantity * product.Value;
 
